9c95c14b087e6a0a60ba2f3965eaef27945b27ee
Tony Ramírez <tonyramirez.business@outlook.com>
2022-09-22 17:11:36 -0500

    added Country Select list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
     <DataTitle :text="title" :dataDate="dataDate" />
 
     <DataBoxes :stats="stats" />
+
+    <CountrySelect :countries="countries" />
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -15,12 +17,14 @@
 <script>
 import DataTitle from '@/components/DataTitle'
 import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 
 export default {
   name: 'Home',
   components: {
     DataTitle,
-    DataBoxes
+    DataBoxes,
+    CountrySelect
   },
   data() {
     return {
