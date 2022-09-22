a3703a17f8295abcd182b3c84fc42bb17944d5e3
Tony Ramírez <tonyramirez.business@outlook.com>
2022-09-22 17:27:16 -0500

    info updated on select country

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -15,14 +15,14 @@
 
     <!-- Box 2 -->
     <div class="shadow-md bg-blue-100 p-10 text-center rounded">
-      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
       <div class="text-2xl mb-4">
         <span class="font-bold">New:</span>
-        {{ numberWithCommas(stats.NewConfirmed) }}
+        {{ numberWithCommas(stats.NewDeaths) }}
       </div>
       <div class="text-2xl mb-4">
         <span class="font-bold">Total:</span>
-        {{ numberWithCommas(stats.TotalConfirmed) }}
+        {{ numberWithCommas(stats.TotalDeaths) }}
       </div>
     </div>
   </div>
